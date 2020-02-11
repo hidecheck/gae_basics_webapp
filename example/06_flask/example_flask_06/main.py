e11be913f58173b72df821b19a893e3dec2fefb9
hidecheck <hidecheck@gmail.com>
2020-02-11 17:14:16 +0900

    fix #1, #3 example_flask_06/ 修正, example/flask04-05

--- example/06_flask/example_flask_06/main.py
+++ example/06_flask/example_flask_06/main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     message = 'Hello World!'
     return render_template('index.html', message=message)
